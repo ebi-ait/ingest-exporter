21085b8b1c43c0c99021b7909bf0ca5792e1ddf1
Amnon Khen <amnon@ebi.ac.uk>
2022-06-14 23:14:11 +0100

    dcp-798 validation for generated links.json file using the json schema

--- tests/exporter/terra/test_dcp_staging_client.py
+++ tests/exporter/terra/test_dcp_staging_client.py
@@ -4,47 +4,29 @@ from unittest.mock import MagicMock, Mock
 from assertpy import assert_that
 from ingest.api.ingestapi import IngestApi
 
-from exporter.metadata import MetadataResource, MetadataParseException
+from exporter.graph.graph_crawler import GraphCrawler
+from exporter.metadata import MetadataResource, MetadataParseException, MetadataService
 from exporter.schema import SchemaService
 from exporter.terra.dcp_staging_client import DcpStagingClient
 from tests.mocks.files import MockEntityFiles
-from tests.mocks.ingest import MockIngestAPI
 
 
-class FileDescriptorTest(TestCase):
-    def setUp(self) -> None:
-        # Setup Entity Files Utility
-        self.ingest = self.init_mock_ingest()
-        self.schema_service = self.init_schema_service(self.ingest)
-        self.mock_files = MockEntityFiles(base_uri='http://mock-ingest-api/')
+class AbstractFileGenerationBaseTests:
+    schema_url = 'not_set'
 
-    def test_when__null_dataFileUuid__fail(self):
-        file_metadata = self.create_invalid_file()
-        dcp_staging_client = self.init_dcp_staging_client()
-        assert_that(dcp_staging_client.generate_file_desciptor_json) \
-            .raises(MetadataParseException) \
-            .when_called_with(file_metadata)
-
-    def test_when__valid_file__pass(self):
-        file_metadata = self.create_valid_file()
-        dcp_staging_client = self.init_dcp_staging_client()
-        dcp_staging_client.generate_file_desciptor_json(file_metadata)
-
-    def create_invalid_file(self) -> MetadataResource:
-        file = self.create_valid_file()
-        file.full_resource["dataFileUuid"] = None
-        return file
-
-    def create_valid_file(self) -> MetadataResource:
-        file_metadata = MetadataResource.from_dict(self.mock_files.get_entity('files', 'mock-analysis-output-file'))
-        return file_metadata
-
-    def init_dcp_staging_client(self):
+    def init_dcp_staging_client(self) -> DcpStagingClient:
         dcp_staging_client = DcpStagingClient(schema_service=self.schema_service,
                                               ingest_client=None,
                                               gcs_storage=None,
                                               gcs_xfer=None)
         return dcp_staging_client
+
+    def setUp(self) -> None:
+        # Setup Entity Files Utility
+        self.ingest = self.init_mock_ingest()
+        self.schema_service = self.init_schema_service(self.ingest)
+        self.dcp_staging_client: DcpStagingClient = self.init_dcp_staging_client()
+        self.mock_files = MockEntityFiles(base_uri='http://mock-ingest-api/')
 
     def init_schema_service(self, mock_ingest_api):
         test_ttl_seconds = 3
@@ -56,8 +38,63 @@ class FileDescriptorTest(TestCase):
         mock_ingest_api.get_schemas = Mock()
         mock_ingest_api.get_schemas.return_value = [
             {
-                "_links": {"json-schema": {"href": "https://schema.humancellatlas.org/system/2.0.0/file_descriptor"}},
+                "_links": {"json-schema": {"href": self.schema_url}},
                 "schemaVersion": "2.0.0"
             }
         ]
         return mock_ingest_api
+
+    def create_invalid_document(self):
+        self.fail('should be implemented by sub classes')
+
+    def create_valid_file(self):
+        self.fail('should implemented by sub classes')
+
+    def test_when__invalid_document__fail(self):
+        file_generation_input = self.create_invalid_document()
+        assert_that(self.file_generation_function) \
+            .raises(MetadataParseException) \
+            .when_called_with(file_generation_input)
+
+    def test_when__valid_document__pass(self):
+        file_metadata = self.create_valid_file()
+        self.file_generation_function(file_metadata)
+
+
+class LinksJsonTest(AbstractFileGenerationBaseTests, TestCase):
+
+    schema_url = "https://schema.humancellatlas.org/system/3.0.0/links"
+
+    def setUp(self):
+        super().setUp()
+        self.file_generation_function = self.dcp_staging_client.generate_links_json
+
+    def create_invalid_document(self):
+        file = self.create_valid_file()
+        list(file.links.values())[0].process_uuid = 'invalid_uuid'
+        return file
+
+    def create_valid_file(self):
+        test_assay_process = MetadataResource.from_dict(self.mock_files.get_entity('processes', 'mock-assay-process'))
+        test_project = MetadataResource.from_dict(self.mock_files.get_entity('projects', 'mock-project'))
+        crawler = GraphCrawler(MetadataService(self.ingest))
+        experiment_graph = crawler.generate_complete_experiment_graph(test_assay_process, test_project)
+        return experiment_graph.links
+
+
+class FileDescriptorTest(AbstractFileGenerationBaseTests, TestCase):
+
+    schema_url = "https://schema.humancellatlas.org/system/2.0.0/file_descriptor"
+
+    def setUp(self):
+        super().setUp()
+        self.file_generation_function = self.dcp_staging_client.generate_file_descriptor_json
+
+    def create_invalid_document(self):
+        file = self.create_valid_file()
+        file.full_resource["dataFileUuid"] = None
+        return file
+
+    def create_valid_file(self):
+        file_metadata = MetadataResource.from_dict(self.mock_files.get_entity('files', 'mock-analysis-output-file'))
+        return file_metadata
