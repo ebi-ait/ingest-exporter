0cb339884783ed2f6bc8f308b755030d20a67f28
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-09-13 16:25:05 +0100

    dcp-815 transfer responder implementation

--- exporter/terra/submission/responder.py
+++ exporter/terra/submission/responder.py
@@ -1,5 +1,6 @@
 import json
 
+from google.api_core.exceptions import AlreadyExists
 from google.cloud.pubsub_v1 import SubscriberClient
 from google.cloud.pubsub_v1.subscriber.message import Message
 from google.oauth2.service_account import Credentials
@@ -9,21 +10,31 @@ from exporter.session_context import SessionContext
 
 
 class TerraTransferResponder:
-    def __init__(self, ingest_service: IngestService, gcp_project_id: str, gcp_topic: str, gcp_credentials_path: str):
+    def __init__(self, ingest_service: IngestService, gcp_project: str, gcp_topic: str, gcp_credentials_path: str):
         self.ingest = ingest_service
-        self.project_id = gcp_project_id
-        self.topic = gcp_topic
+        self.subscription_path = SubscriberClient.subscription_path(gcp_project, gcp_topic)
+        self.topic_path = SubscriberClient.topic_path(gcp_project, gcp_topic)
         self.logger = SessionContext.register_logger(__name__)
 
         with open(gcp_credentials_path) as source:
             credentials_file = json.load(source)
-        credentials = Credentials.from_service_account_info(credentials_file)
-        subscriber = SubscriberClient(credentials=credentials)
-        subscription_path = subscriber.subscription_path(self.project_id, self.topic)
-        self.future = subscriber.subscribe(subscription_path, callback=self.handle_message)
+        self.credentials = Credentials.from_service_account_info(credentials_file)
+        with SubscriberClient(credentials=self.credentials) as subscriber:
+            try:
+                subscriber.create_subscription(name=self.subscription_path, topic=self.topic_path)
+            except AlreadyExists:
+                self.logger.info(f'Congratulations! Subscription Exists: {self.subscription_path}')
+            except Exception as e:
+                self.logger.error(f'Error checking whether subscription exists: {self.subscription_path}, {e}')
 
     def listen(self):
-        self.future.result()
+        with SubscriberClient(credentials=self.credentials) as subscriber:
+            future = subscriber.subscribe(self.subscription_path, callback=self.handle_message)
+            try:
+                self.logger.info(f'Running Google Data Transfer Listener')
+                future.result()
+            except Exception:
+                future.cancel()
 
     def handle_message(self, message: Message):
         if message.attributes.get("eventType", "") != "TRANSFER_OPERATION_SUCCESS":
@@ -31,7 +42,7 @@ class TerraTransferResponder:
         transfer_name = message.attributes.get("transferJobName", "")
         if not transfer_name.startswith('transferJobs/'):
             message.nack()
-        export_job_id = transfer_name.removeprefix('transferJobs/')
+        export_job_id = transfer_name.replace('transferJobs/', '')
         self.logger.info(f'Received message from google that data transfer for export job {export_job_id} is finished.')
         self.ingest.set_data_file_transfer(export_job_id, 'COMPLETE')
         message.ack()
