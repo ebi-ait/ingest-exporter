02a7b2bd7fe26555842cdbf70e769ec0685e33f1
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-11-25 16:47:24 +0000

    requeue errors fixup

--- docs/requeue_errors.py
+++ docs/requeue_errors.py
@@ -3,19 +3,21 @@ from kombu import Connection, Exchange, Queue, Consumer, Message, Producer
 exchange = Exchange('ingest.exporter.exchange', type="topic")
 error_queue = Queue(name='ingest.exporter.errored.queue', exchange=exchange, routing_key='ingest.terra.spreadsheet.error')
 conn = Connection("amqp://localhost:5672/")
-with conn.channel() as channel:
-    def process_errors(body: str, message: Message):
-        routing_key = message.headers.get('x-death', [{}])[0].get('routing-keys', [''])[0]
-        if routing_key:
-            print(f"message re-published to {routing_key}: {body}")
-            producer = Producer(exchange=exchange, channel=channel, routing_key=routing_key)
-            producer.publish(body)
-            message.ack()
-        else:
-            print(f"message rejected, no routing key: {body}")
-            message.reject(requeue=True)
+channel = conn.channel()
 
-    with Consumer(conn, queues=error_queue, callbacks=[process_errors], accept=["application/json;charset=UTF-8"]):
-        conn.drain_events(timeout=10)
 
+def process_errors(body: str, message: Message):
+    routing_key = message.headers.get('x-death', [{}])[0].get('routing-keys', [''])[0]
+    if routing_key:
+        print(f"message re-published to {routing_key}: {body}")
+        producer = Producer(exchange=exchange, channel=channel, routing_key=routing_key)
+        producer.publish(body)
+        message.ack()
+    else:
+        message.nack()
+
+
+with Consumer(conn, queues=error_queue, callbacks=[process_errors], accept=["application/json;charset=UTF-8"]):
+    conn.drain_events(timeout=2)
     channel.close()
+
